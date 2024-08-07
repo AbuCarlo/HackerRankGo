05c8da33473d80a48c3c7b3fac2cf580ae4e8231
Tony Nassar <anthonyabunassar@gmail.com>
2024-08-06 23:30:02 -0400

    Determined that slices won't work as friend groups.

--- various/miscellaneous/FriendCircle_test.go
+++ various/miscellaneous/FriendCircle_test.go
@@ -5,27 +5,32 @@ import (
 )
 
 func FriendCircle(queries [][]int) []int {
-	friends := map[int]map[int]bool{}
+	friends := map[int][]int{}
 	max := 0
 	result := make([]int, len(queries))
 	for i, q := range queries {
+		// Is one of these nodes already in the map?
 		left, right := q[0], q[1]
 		_, ok := friends[left]
 		if !ok {
-			friends[left] = map[int]bool{ left: true }
+			friends[left] = []int{left}
 		}
 		_, ok = friends[right]
 		if !ok {
-			friends[right] = map[int]bool{ right: true }
+			friends[right] = []int{right}
 		}
-		for friend := range friends[left] {
-			friends[right][friend] = true
+		// If left and right are already friends, they
+		// point to equivalent slices. We can't check
+		// reference equality in Go: a slice is a value 
+		// object.
+		friends[left] = append(friends[left], friends[right]...)
+		for _, friend := range friends[left] {
+			if friend != left {
+				friends[friend] = friends[left]
+			}
 		}
-		for friend := range friends[right] {
-			friends[friend] = friends[right]
-		}
-		if len(friends[right]) > max {
-			max = len(friends[right])
+		if len(friends[left]) > max {
+			max = len(friends[left])
 		}
 		result[i] = max
 	}
@@ -33,6 +38,14 @@ func FriendCircle(queries [][]int) []int {
 }
 
 func TestFriendCircle(t *testing.T) {
+	input00 := [][]int{
+		{1, 2},
+		{1, 3},
+	}
+
+	output00 := FriendCircle(input00)
+	t.Logf("%v", output00)
+
 	input := [][]int{
 		{1, 2},
 		{3, 4},
@@ -42,4 +55,20 @@ func TestFriendCircle(t *testing.T) {
 	output := FriendCircle(input)
 
 	t.Logf("%v", output)
+
+	input02 := [][]int{
+		{6, 4},
+		{5, 9},
+		{8, 5},
+		{4, 1},
+		{1, 5},
+		{7, 2},
+		{4, 2},
+		{7, 6},
+	}
+
+	output02 := FriendCircle(input02)
+
+	t.Logf("%v", output02)
+
 }
