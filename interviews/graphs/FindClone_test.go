25c90ab42a14834f39c656a06954d27d5e99e1d1
Tony Nassar <anthonyabunassar@gmail.com>
2024-08-21 13:48:08 -0400

    Time limit exceeded.

--- interviews/graphs/FindClone_test.go
+++ interviews/graphs/FindClone_test.go
@@ -10,11 +10,11 @@ type ColoredGraph struct {
 	order int32
 	adjacency map[int32]*Set[int32]
 	// This seems insane.
-	colors map[int32]int32
+	colors map[int32]int64
 }
 
 func NewColoredGraph(order int32) *ColoredGraph {
-	g := ColoredGraph{order, make(map[int32]*Set[int32]), make(map[int32]int32)}
+	g := ColoredGraph{order, make(map[int32]*Set[int32]), make(map[int32]int64)}
 	return &g
 }
 
@@ -29,13 +29,13 @@ func (g *ColoredGraph) AddEdge(u, v int32) {
 	g.adjacency[v].Add(u)
 }
 
-func (g *ColoredGraph) Color(colors... int32) {
+func (g *ColoredGraph) Color(colors... int64) {
 	for i, color := range colors {
 		g.colors[int32(i + 1)] = color
 	}
 }
 
-func (g *ColoredGraph) FindClone(color int32) int64 {
+func (g *ColoredGraph) FindClone(color int64) int64 {
 	distances := g.FloydWarshall()
 	result := int64(math.MaxInt64)
 	for v := int32(1); v <= g.order; v++ {
@@ -92,29 +92,40 @@ func (g *ColoredGraph) FloydWarshall() [][]int64 {
 	return distances
 }
 
+
+func ConstructTestCase(order int32, from []int32, to []int32, colors []int64) *ColoredGraph {
+	g := NewColoredGraph(order)
+	for j, u := range from {
+		g.AddEdge(u, to[j])
+	}
+	g.Color(colors...)
+	return g
+}
+
+func findShortest(graphNodes int32, graphFrom []int32, graphTo []int32, ids []int64, val int32) int32 {
+    g := ConstructTestCase(graphNodes, graphFrom, graphTo, ids)
+	return int32(g.FindClone(int64(val)))
+}
+
 func TestFindCloneSamples(t *testing.T) {
 	testCases := []struct {
 		order int32
 		from []int32
 		to []int32
-		colors []int32
+		colors []int64
 		clone int32
 		expected int32
 	}{
 		// free samples
-		{ 4, []int32{1, 1, 2}, []int32{2, 3, 4}, []int32{1, 2, 1, 1 }, 1, 1 },
-		{ 4, []int32{1, 1, 4}, []int32{2, 3, 2}, []int32{1, 2, 3, 4}, 2, -1 },
-		{ 5, []int32{1, 1, 2, 3}, []int32{2, 3, 4, 5}, []int32{1, 2, 3, 3, 2}, 2, 3 },
+		{ 4, []int32{1, 1, 2}, []int32{2, 3, 4}, []int64{1, 2, 1, 1 }, 1, 1 },
+		{ 4, []int32{1, 1, 4}, []int32{2, 3, 2}, []int64{1, 2, 3, 4}, 2, -1 },
+		{ 5, []int32{1, 1, 2, 3}, []int32{2, 3, 4, 5}, []int64{1, 2, 3, 3, 2}, 2, 3 },
 		// Test Case 1
 	}
 
 	for i, test := range testCases {
-		g := NewColoredGraph(test.order)
-		for j, u := range test.from {
-			g.AddEdge(u, test.to[j])
-		}
-		g.Color(test.colors...)
-		actual := g.FindClone(test.clone)
+		g := ConstructTestCase(test.order, test.from, test.to, test.colors)
+		actual := g.FindClone(int64(test.clone))
 		if actual != int64(test.expected) {
 			t.Errorf("Test %d expected %d, found %d", i, test.expected, actual)
 		} else {
@@ -122,5 +133,6 @@ func TestFindCloneSamples(t *testing.T) {
 		}
 	}
 }
+
 
 
