88d4ff0c82521c01f30fb4215ee6109c7ab0b252
Tony Nassar <anthonyabunassar@gmail.com>
2024-08-28 20:45:35 -0400

    WIP

--- interviews/graphs/matrix.py
+++ interviews/graphs/matrix.py
@@ -8,47 +8,72 @@
 # by both the respective paths, since those two 
 # machines would still be connected to each other.
 
+import heapq
+import os
+
+
 class Graph:
-    def __init__(self, cities, machines):
-        self.adjacency = [None] * len(cities)
-        self.cities = set(machines)
+    def __init__(self, cities):
+        self.adjacency = {}
+        for (u, v, cost) in cities:
+            self.connect(u, v, cost)
 
     def connect(self, u, v, cost):
-        if self.adjacency[u] is None:
+        if u not in self.adjacency:
             self.adjacency[u] = {}
-        if self.adjacency[v] is None:
+        if v not in self.adjacency:
             self.adjacency[v] = {}
         self.adjacency[u][v] = cost
         self.adjacency[v][u] = cost
         
-    def find_all_distances(self, source):
-        queue = set()
+    def disconnect(self, u, v):
+        del(self.adjacency[v], u)
+        del(self.adjacency[u], v)
+        
+    def find_shortest_path(self, source, target):
+        queue = []
         distances = {}
+        previous = {}
         visited = set()
-        queue.add(source)
+        
+        distances[source] = 0
+        heapq.heappush(queue, (0, source))
+
         while queue:
-            u = queue.pop(0)
-            for v in self.adjacency[v]:
+            _, u = heapq.heappop(queue)
+            if u == target:
+                break
+            for v in self.adjacency[u]:
                 if v in visited:
                     continue
-                queue.add(v)
-                alt = distances[u] + 1
+                alt = distances[u] + self.adjacency[u][v]
                 if v not in distances:
                     distances[v] = alt
-                else:
-                    distances[v] += 1
+                    previous[v] = u
+                elif distances[v] > alt:
+                    distances[v] = alt
+                    previous[v] = u
+                heapq.heappush(queue, (distances[v], v))
                 
             visited.add(u)
-            
-        return [distances.get(v, -1) for v in range(len(self.adjacency))]
+
+        v = target
+        path = []
+        while v != source:
+            path = [v] + path
+            v = previous[v]
+        return path
+    
+def minTime(roads, machines):
+    graph = Graph(roads)
+    for machine in machines:
+        for other_machine in [m for m in machines if m != machine]:
+            previous = graph.find_shortest_path(machine, other_machine)
+            # What is the lowest-cost edge on this path?
+            edges = [(previous[i - 1], previous[i]) for i in range(1, len(previous))]
+            print(edges)
 
 
-t = int(input())
-for i in range(t):
-    n,m = [int(value) for value in input().split()]
-    graph = Graph(n)
-    for i in range(m):
-        x,y = [int(x) for x in input().split()]
-        graph.connect(x-1,y-1) 
-    s = int(input())
-    graph.find_all_distances(s-1)
+# Sample 0
+minTime([[2, 1, 8], [1, 0, 5], [2, 4, 5], [1, 3, 4]], [2, 4, 0])
+
