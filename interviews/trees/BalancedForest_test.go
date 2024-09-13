0307db1f6c94ecdea272e294f47c0cbe8fdd9489
Tony Nassar <anthonyabunassar@gmail.com>
2024-09-13 09:12:59 -0400

    Sketching unit test

--- interviews/trees/BalancedForest_test.go
+++ interviews/trees/BalancedForest_test.go
@@ -1,14 +1,20 @@
 package trees
 
 import (
-	"pgregory.net/rapid"
+	"bufio"
+	"io"
+	"os"
+	"strconv"
+	"strings"
 	"testing"
+
+	"pgregory.net/rapid"
 )
 
 type Node struct {
-	Id int
-	Value int
-	Parent *Node
+	Id       int
+	Value    int
+	Parent   *Node
 	Children []*Node
 	// TODO: Make this a long.
 	Sum int
@@ -24,12 +30,32 @@ func wire(node *Node) {
 	node.Sum = sum
 }
 
+func Disconnect(node *Node) *Node {
+	var result *Node
+	for parent := node.Parent; parent != nil; {
+		result = &Node{parent.Id, parent.Value, nil, nil, parent.Sum - node.Sum}
+		// Copy the children, with one exception.
+		for _, n := range parent.Children {
+			if n == node {
+				result.Children = append(result.Children, result)
+				n.Parent = result
+			} else {
+				result.Children = append(result.Children, n)
+			}
+		}
+		tmp := parent
+		parent = parent.Parent
+		node = tmp
+	}
+	return result
+}
+
 func TestTreeGeneration(t *testing.T) {
 	f := func(t *rapid.T) {
 		size := rapid.IntRange(1, 100).Draw(t, "size")
 
-		valueGenerator := rapid.IntRange(1, 100);
-		blahGenerator := rapid.IntRange(1, 3);
+		valueGenerator := rapid.IntRange(1, 100)
+		blahGenerator := rapid.IntRange(1, 3)
 
 		id := 1
 		c := valueGenerator.Draw(t, "c")
@@ -69,7 +95,7 @@ func TestTreeGeneration(t *testing.T) {
 			}
 
 			if sum != node.Sum {
-				t.Errorf("Node %d has a sum of %d, but its children sum to %d", node.Id, node.Sum, sum);
+				t.Errorf("Node %d has a sum of %d, but its children sum to %d", node.Id, node.Sum, sum)
 			}
 
 		}
@@ -77,3 +103,77 @@ func TestTreeGeneration(t *testing.T) {
 	rapid.Check(t, f)
 }
 
+func balancedForest(_ *Node) int {
+	return 0
+}
+
+func TestSamples(t *testing.T) {
+	type Test struct {
+		path     string
+		expected int
+	}
+
+	tests := []Test{
+		{"input00-1.txt", 1},
+		{"input00-2.txt", 1},
+		{"input06.txt", 1},
+		{"input07.txt", 1},
+	}
+
+	for _, test := range tests {
+		tree := read("./balanced-forest-inputs" + "/" + test.path)
+		actual := balancedForest(tree)
+		if actual != test.expected {
+			t.Errorf("Test of %s expected %d; was %d", test.path, test.expected, actual)
+		}
+	}
+
+}
+
+func read(path string) *Node {
+	// This is basically the code from HackerRank.
+	f, err := os.Open(path)
+	checkError(err)
+	reader := bufio.NewReaderSize(f, 16*1024*1024)
+
+	nTemp, err := strconv.ParseInt(strings.TrimSpace(readLine(reader)), 10, 64)
+	checkError(err)
+	n := int(nTemp)
+
+	nodes := make([]*Node, n + 1)
+
+	cTemp := strings.Split(strings.TrimSpace(readLine(reader)), " ")
+
+	for i := range n {
+		cItemTemp, err := strconv.ParseInt(cTemp[i], 10, 64)
+		checkError(err)
+		cItem := int(cItemTemp)
+		id := i + 1
+		nodes[id] = &Node{id, cItem, nil, nil, 0}
+	}
+
+	for i := 0; i < int(n)-1; i++ {
+		a := strings.Split(strings.TrimRight(readLine(reader), " \t\r\n"), " ")
+		parent, _ := strconv.ParseInt(a[0], 10, 32)
+		child, _ := strconv.ParseInt(a[1], 10, 32)
+		nodes[child].Parent = nodes[parent]
+		nodes[parent].Children = append(nodes[parent].Children, nodes[child])
+	}
+
+	return nodes[1]
+}
+
+func readLine(reader *bufio.Reader) string {
+	str, _, err := reader.ReadLine()
+	if err == io.EOF {
+		return ""
+	}
+
+	return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
