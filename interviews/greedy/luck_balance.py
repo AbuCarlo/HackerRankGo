e86548a78807a3802a75c30cf8088cd2ce1fee1f
Tony Nassar <anthonyabunassar@gmail.com>
2024-09-27 15:19:40 -0400

    Sorting array

--- interviews/greedy/luck_balance.py
+++ interviews/greedy/luck_balance.py
@@ -1,17 +1,21 @@
 
 sample_0 = [[5, 1], [2, 1], [1, 1], [8, 1], [10, 0], [5, 0]]
 
-def luckBalance(k, c):
+def recurse(k, c):
     if not c:
         return 0
     if k == 0:
         return sum([v for v, important in c if not important]) + sum([-v for v, important in c if important])
     v, important = c[0]
     if not important:
-        return v + luckBalance(k, c[1:])
-    x = v + luckBalance(k - 1, c[1:])
-    y = luckBalance(k, c[1:]) - v
+        return v + recurse(k, c[1:])
+    x = v + recurse(k - 1, c[1:])
+    y = recurse(k, c[1:]) - v
     return max(x, y)
+
+def luckBalance(k, c):
+    c = sorted(c, key=lambda t: t[0])
+    return recurse(k, c)
 
 print(luckBalance(3, sample_0))
 
