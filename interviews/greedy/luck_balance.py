55141a351d7aa7146626f8f5952d7fda4f1c95bc
Tony Nassar <anthonyabunassar@gmail.com>
2024-09-27 15:30:31 -0400

    Memoization FTW

--- interviews/greedy/luck_balance.py
+++ interviews/greedy/luck_balance.py
@@ -1,22 +1,28 @@
 
 sample_0 = [[5, 1], [2, 1], [1, 1], [8, 1], [10, 0], [5, 0]]
 
-def recurse(k, c):
+def recurse(m, k, c):
     if not c:
         return 0
+    key = (k, len(c))
+    if key in m:
+        return m[key]
     if k == 0:
-        return sum([v for v, important in c if not important]) + sum([-v for v, important in c if important])
+        result = sum([v for v, important in c if not important]) + sum([-v for v, important in c if important])
+        m[key] = result
+        return result
     v, important = c[0]
     if not important:
-        return v + recurse(k, c[1:])
-    x = v + recurse(k - 1, c[1:])
-    y = recurse(k, c[1:]) - v
-    return max(x, y)
+        return v + recurse(m, k, c[1:])
+    x = v + recurse(m, k - 1, c[1:])
+    y = recurse(m, k, c[1:]) - v
+    result = max(x, y)
+    m[key] = result
+    return result
 
 def luckBalance(k, c):
-    # larger values first
-    c = sorted(c, key=lambda t: -t[0])
-    return recurse(k, c)
+    m = {}
+    return recurse(m, k, c)
 
 print(luckBalance(3, sample_0))
 
